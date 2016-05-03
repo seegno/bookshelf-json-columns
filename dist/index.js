95b55308163308ae4b32aaeba11fb18cc346ae93
rummik <r@9k1.us>
2016-05-03 16:30:14 -0400

    Parse JSON when fetched

--- dist/index.js
+++ dist/index.js
@@ -8,33 +8,45 @@
 Object.defineProperty(exports, '__esModule', {
   value: true
 });
+function parse() {
+  var _this = this;
+
+  this.jsonColumns.forEach(function (column) {
+    if (_this.attributes[column]) {
+      _this.attributes[column] = JSON.parse(_this.attributes[column]);
+    }
+  });
+}
+
+function stringify() {
+  var _this2 = this;
+
+  this.jsonColumns.forEach(function (column) {
+    if (_this2.attributes[column]) {
+      _this2.attributes[column] = JSON.stringify(_this2.attributes[column]);
+    }
+  });
+}
 
 exports['default'] = function (Bookshelf) {
   var Model = Bookshelf.Model.prototype;
 
   Bookshelf.Model = Bookshelf.Model.extend({
     initialize: function initialize() {
-      var _this = this;
-
       if (!this.jsonColumns) {
         return Model.initialize.apply(this, arguments);
       }
 
       // Stringify JSON columns before saving.
-      this.on('saving', function () {
-        _this.jsonColumns.forEach(function (column) {
-          if (_this.attributes[column]) {
-            _this.attributes[column] = JSON.stringify(_this.attributes[column]);
-          }
-        });
-      });
+      this.on('saving', stringify.bind(this));
 
-      // Parse JSON columns after saving.
-      this.on('saved', function () {
-        _this.jsonColumns.forEach(function (column) {
-          if (_this.attributes[column]) {
-            _this.attributes[column] = JSON.parse(_this.attributes[column]);
-          }
+      // Parse JSON columns after saving and after fetched.
+      this.on('saved fetched', parse.bind(this));
+
+      // Parse JSON after collection is fetched.
+      this.on('fetched:collection', function (collection) {
+        collection.models.forEach(function (model) {
+          parse.call(model);
         });
       });
 
