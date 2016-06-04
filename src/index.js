abf5e93d67fc8ff53442022c44801761c8087735
Ricardo Nogueira <rgn@seegno.com>
2016-06-04 21:51:40 +0100

    Add json columns parsing with SQLite client

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
 
 /**
+ * Stringify JSON columns.
+ */
+
+function stringify() {
+  this.jsonColumns.forEach(column => {
+    if (this.attributes[column]) {
+      this.attributes[column] = JSON.stringify(this.attributes[column]);
+    }
+  });
+}
+
+/**
+ * Parse JSON columns.
+ */
+
+function parse() {
+  this.jsonColumns.forEach(column => {
+    if (this.attributes[column]) {
+      this.attributes[column] = JSON.parse(this.attributes[column]);
+    }
+  });
+}
+
+/**
  * Export `bookshelf-json-columns` plugin.
  */
 
 export default Bookshelf => {
   const Model = Bookshelf.Model.prototype;
+  const client = Bookshelf.knex.client.config.client;
 
   Bookshelf.Model = Bookshelf.Model.extend({
     initialize() {
@@ -12,25 +37,39 @@ export default Bookshelf => {
         return Model.initialize.apply(this, arguments);
       }
 
-      // Stringify JSON columns before saving.
-      this.on('saving', () => {
-        this.jsonColumns.forEach(column => {
-          if (this.attributes[column]) {
-            this.attributes[column] = JSON.stringify(this.attributes[column]);
-          }
-        });
-      });
+      // Stringify JSON columns before model is saved.
+      this.on('saving', stringify.bind(this));
 
-      // Parse JSON columns after saving.
-      this.on('saved', () => {
-        this.jsonColumns.forEach(column => {
-          if (this.attributes[column]) {
-            this.attributes[column] = JSON.parse(this.attributes[column]);
-          }
-        });
-      });
+      // Parse JSON columns after model is saved.
+      this.on('saved', parse.bind(this));
+
+      if (client === 'sqlite') {
+        // Parse JSON columns after model is fetched.
+        this.on('fetched', parse.bind(this));
+      }
 
       return Model.initialize.apply(this, arguments);
     }
   });
+
+  if (client === 'sqlite') {
+    const Collection = Bookshelf.Collection.prototype;
+
+    Bookshelf.Collection = Bookshelf.Collection.extend({
+      initialize() {
+        if (!this.model.prototype.jsonColumns) {
+          return Collection.initialize.apply(this, arguments);
+        }
+
+        // Parse JSON columns after collection is fetched.
+        this.on('fetched', collection => {
+          collection.models.forEach(model => {
+            parse.apply(model);
+          });
+        });
+
+        return Collection.initialize.apply(this, arguments);
+      }
+    });
+  }
 }
