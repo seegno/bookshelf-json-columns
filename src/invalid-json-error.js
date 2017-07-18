71b6d7529ae6124099209c3a8d667ea289f03bdb
Ricardo Gama <rgama@uphold.com>
2017-07-18 14:46:14 +0100

    Add InvalidJSONError

---
+++ src/invalid-json-error.js
@@ -0,0 +1,12 @@
+
+/**
+ * Module dependencies.
+ */
+
+import StandardError from 'standard-error';
+
+/**
+ * Export `InvalidJSONError`.
+ */
+
+export default class InvalidJSONError extends StandardError {}
